b62c5f788d89d81074b8c2e9674919cf69f8e648
Mattia Sanvito <mattiasanvito@hotmail.it>
2023-07-28 04:48:29 +0200

    Added temporary files to be completed

---
+++ pages/01_Situazione.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+from utils import helpers
+
+helpers.page_init('Situazione')
+
+st.subheader('Work in progress...')
---
+++ pages/02_Listone.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+from utils import helpers
+
+helpers.page_init('Listone')
+
+st.subheader('Work in progress...')
