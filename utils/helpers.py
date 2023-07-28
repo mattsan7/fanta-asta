ec6037570201f71d029dba597ea55b37c6616ad7
Mattia Sanvito <mattiasanvito@hotmail.it>
2023-07-28 06:27:50 +0200

    icon change

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,7 +7,7 @@ import streamlit as st
 def page_init(title: str):
     st.set_page_config(
         page_title=f"{title} - Fanta Asta",
-        page_icon="💲",
+        page_icon="💰",
         initial_sidebar_state="auto",
         menu_items=None,
     )
